19d40c018a8dc21e9f11a0281daae8f5bc2d09fa
Jonathan Ellington <jellingt@meraki.com>
2020-01-13 19:09:06 -0800

    separate classes in dashboard.rb
    dashboard.rb -> organization.rb
                    network.rb
                    device.rb

--- lib/meraki.rb
+++ lib/meraki.rb
@@ -1,4 +1,6 @@
-require 'meraki/dashboard'
+require 'meraki/organization'
+require 'meraki/network'
+require 'meraki/device'
 require 'meraki/api'
 require 'meraki/version'
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,5 @@
 require 'bundler/setup'
 require 'byebug'
-
 require 'meraki'
 
 RSpec.configure do |config|
