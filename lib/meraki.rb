17cfafbfda96b89c9b9e239a0345e09a7d86e8a8
Jonathan Ellington <jellingt@meraki.com>
2020-01-13 19:23:34 -0800

    remove unused ipaddress gem, sort requires

--- lib/meraki.rb
+++ lib/meraki.rb
@@ -1,7 +1,7 @@
-require 'meraki/organization'
-require 'meraki/network'
-require 'meraki/device'
 require 'meraki/api'
+require 'meraki/device'
+require 'meraki/network'
+require 'meraki/organization'
 require 'meraki/version'
 
 module Meraki
